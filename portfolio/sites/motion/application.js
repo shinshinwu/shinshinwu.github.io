5c911adb14a350a23f500dd089985cc87c37b8bd
Anna Wu <shinshinwu@gmail.com>
2015-01-02 14:38:42 -0800

    Finished motion site

--- portfolio/sites/motion/application.js
+++ portfolio/sites/motion/application.js
@@ -1,14 +1,14 @@
 var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
-ctx.globalCompositeOperation = 'lighter ';
+ctx.globalCompositeOperation = 'lighter';
 
-ctx.rect(50, 10, 500, 500);
+ctx.rect(68, 10, 800, 800);
 ctx.fillStyle = 'rgb(200,200,200)';
 ctx.fill();
 ctx.globalCompositeOperation = 'destination-out';
 
 ctx.fillStyle = 'rgba(255,255,255,1)';
-ctx.font="120px Raleway";
+ctx.font="106px Raleway";
 ctx.fillText("MOTION",50,150);
-ctx.fillText("IS",83,230);
-ctx.fillText("LIFE",229,230);
+ctx.fillText("IS",120,227);
+ctx.fillText("LIFE",270,227);
